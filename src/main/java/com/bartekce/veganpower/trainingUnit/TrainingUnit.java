b4ea8170b16c65ff905948d5b790b5836360b1ba
Bartosz Celuch <bartosz.celuch@eclipsegroup.co.uk>
2021-07-04 12:50:24 +0200

    :hamm: Change  parameter Date to LocalDate

--- src/main/java/com/bartekce/veganpower/trainingUnit/TrainingUnit.java
+++ src/main/java/com/bartekce/veganpower/trainingUnit/TrainingUnit.java
@@ -2,7 +2,7 @@ package com.bartekce.veganpower.trainingUnit;
 
 import com.bartekce.veganpower.exercises.Exercise;
 
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 public class TrainingUnit {
@@ -11,14 +11,17 @@ public class TrainingUnit {
     private List<Exercise> exercises;
 
     private int sets;
-    private Date date;
+    private LocalDate date;
 
 
-    public TrainingUnit(Long id, List<Exercise> exercises, int sets, Date date) {
+
+
+    public TrainingUnit(Long id, List<Exercise> exercises, int sets, LocalDate date) {
         this.id = id;
         this.exercises = exercises;
         this.sets = sets;
         this.date = date;
+
     }
 
     public Long getId() {
@@ -46,11 +49,21 @@ public class TrainingUnit {
         this.sets = sets;
     }
 
-    public Date getDate() {
+    public LocalDate getDate() {
         return date;
     }
 
-    public void setDate(Date date) {
+    public void setDate(LocalDate date) {
         this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "TrainingUnit{" +
+                "id=" + id +
+                ", exercises=" + exercises +
+                ", sets=" + sets +
+                ", date=" + date +
+                '}';
     }
 }
