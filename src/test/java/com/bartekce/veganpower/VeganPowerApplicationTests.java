f3e51af9318fc3bd62b5912e420b65a169bd0c91
Bartosz Celuch <bartosz.celuch@eclipsegroup.co.uk>
2021-07-04 10:21:26 +0200

    :tada: Start the project

---
+++ src/test/java/com/bartekce/veganpower/VeganPowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bartekce.veganpower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeganPowerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
