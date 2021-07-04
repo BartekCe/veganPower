130c738b16708601477df01744db801574042820
Bartosz Celuch <bartosz.celuch@eclipsegroup.co.uk>
2021-07-04 12:47:07 +0200

    :beers: Add TrainingUnitService class

---
+++ src/main/java/com/bartekce/veganpower/trainingUnit/TrainingUnitService.java
@@ -0,0 +1,35 @@
+package com.bartekce.veganpower.trainingUnit;
+
+import com.bartekce.veganpower.exercises.ExerciseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class TrainingUnitService {
+
+    private final ExerciseService exerciseService;
+
+
+
+    @Autowired
+    public TrainingUnitService(ExerciseService exerciseService) {
+        this.exerciseService = exerciseService;
+    }
+
+//    TrainingUnit trainingUnit_1 = new TrainingUnit(1L,exerciseService.getExercises(),2, LocalDate.now());
+
+    public List<TrainingUnit> getTrainingUnits(){
+        return List.of(
+                new TrainingUnit(
+                        1L,
+                        exerciseService.getExercises(),
+                        2,
+                        LocalDate.of(2021,7,4)
+                )
+        );
+    }
+}
+
